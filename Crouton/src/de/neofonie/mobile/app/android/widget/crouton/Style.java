93d95104a7c41f4e72a07e4df40da30f35db59bc
Eitan <Eitan@Eitan-Lap>
2012-08-21 22:45:11 +0300

    test

---
+++ Crouton/src/de/neofonie/mobile/app/android/widget/crouton/Style.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2012 Neofonie Mobile GmbH
+ *	
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package de.neofonie.mobile.app.android.widget.crouton;
+
+import android.view.ViewGroup.LayoutParams;
+
+/**
+ * Style <br>
+ * <br>
+ * 
+ * The style for a {@link Crouton}.
+ * 
+ * @author weiss@neofonie.de
+ * 
+ */
+
+public class Style {
+  public static final Style ALERT   = new Style(5000,
+		  										0xFFFF4444,
+                                                LayoutParams.WRAP_CONTENT);
+  public static final Style CONFIRM = new Style(3000,
+		  										0xFF99CC00,
+                                                LayoutParams.WRAP_CONTENT);
+  public static final Style INFO    = new Style(3000,
+		 										0xFF00DDFF,
+                                                LayoutParams.WRAP_CONTENT);
+  /**
+   * The duration the {@link Crouton} will be displayed in milliseconds.
+   */
+  final int                 duration;
+  /**
+   * The color's int value.
+   */
+  final int                 color;
+  /**
+   * The height of the {@link Crouton} in pixels.
+   */
+  final int                 height;
+  /**
+   * The resource id of the background.
+   * 
+   * 0 for no background.
+   */
+  final int                 background;
+  /**
+   * Whether we should tile the background or not.
+   */
+  final boolean             tile;
+  /**
+   * The text color's resource id.
+   * 
+   * 0 sets the text color to the system theme default.
+   */
+  final int                 textColor;
+
+  /**
+   * Creates a new {@link Crouton} with the provided parameters.
+   * 
+   * @param duration
+   *          The duration the crouton will be displayed {@link Crouton} in milliseconds.
+   * @param color
+   *          The color's resource id.
+   * @param height
+   *          The height of the {@link Crouton}. Either {@link LayoutParams#MATCH_PARENT} or
+   *          {@link LayoutParams#WRAP_CONTENT}.
+   */
+  public Style(int duration, int color, int height) {
+    this(duration, color, height, 0, false, 0);
+  }
+
+  /**
+   * Creates a new {@link Crouton} with the provided parameters
+   * 
+   * @param duration
+   *          The duration the crouton will be displayed {@link Crouton} in milliseconds.
+   * @param color
+   *          The color's resource id.
+   * @param height
+   *          The height of the {@link Crouton}. Either {@link LayoutParams#MATCH_PARENT} or
+   *          {@link LayoutParams#WRAP_CONTENT}.
+   * @param textColor
+   *          The resource id of the text's color.
+   */
+  public Style(int duration, int color, int height, int textColor) {
+    this(duration, color, height, 0, false, textColor);
+  }
+
+  /**
+   * Creates a new {@link Crouton} with the provided parameters.
+   * 
+   * @param duration
+   *          The duration the crouton will be displayed {@link Crouton} in milliseconds.
+   * @param color
+   *          The color's resource id.
+   * @param height
+   *          The height of the {@link Crouton}. Either {@link LayoutParams#MATCH_PARENT} or
+   *          {@link LayoutParams#WRAP_CONTENT}.
+   * @param background
+   *          A background image drawable's resource id.
+   * @param tile
+   *          <code>true</code> if you want the background to be tiled, else <code>false</code>.
+   * @param textColor
+   *          The resource id of the text's color.
+   */
+  public Style(int duration, int color, int height, int background, boolean tile, int textColor) {
+    this.duration = duration;
+    this.color = color;
+    this.height = height;
+    this.background = background;
+    this.tile = tile;
+    this.textColor = textColor;
+  }
+}
+
---
+++ Crouton/src/de/neofonie/mobile/app/android/widget/crouton/ViewHolder.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2012 Neofonie Mobile GmbH
+ *	
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package de.neofonie.mobile.app.android.widget.crouton;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Shader;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * ViewHolder <br>
+ * <br>
+ * <br>
+ * The {@link ViewHolder} contains a view that can be used to display a
+ * {@link Crouton}.
+ * 
+ * @author weiss@neofonie.de
+ * 
+ */
+final class ViewHolder {
+	private static final int PADDING = 10;
+	private static int sDefaultTextColor;
+	private static LayoutParams sParams;
+	private FrameLayout view;
+	private TextView text;
+	private ImageView background;
+
+	private static ViewHolder holder;
+
+	private ViewHolder(Crouton crouton) {
+		if (sParams == null) {
+			sParams = new LayoutParams(
+					LayoutParams.MATCH_PARENT, LayoutParams.FILL_PARENT);
+		}
+		
+		if (sDefaultTextColor == 0) {
+			sDefaultTextColor = new TextView(crouton.getActivity()).getTextColors().getDefaultColor();
+		}
+		
+		initView(crouton);
+	}
+
+	/**
+	 * Creates a view for a {@link Crouton}.
+	 * 
+	 * @param crouton
+	 *            The {@link Crouton} that the view should be attached to.
+	 * @return The view for this {@link Crouton};
+	 */
+	public static View viewForCrouton(Crouton crouton) {
+		if (holder == null) {
+			holder = new ViewHolder(crouton);
+		} else {
+			holder.text.setText(crouton.getText());
+		}
+//		holder.view.setBackgroundColor(crouton.getActivity().getResources()
+//				.getColor(crouton.getStyle().color));
+		holder.view.setBackgroundColor(crouton.getStyle().color);
+		
+		if (crouton.getStyle().textColor != 0) {
+			holder.text.setTextColor(crouton.getActivity().getResources()
+					.getColor(crouton.getStyle().textColor));
+		} else {
+			holder.text.setTextColor(sDefaultTextColor);
+		}
+		
+		if (crouton.getStyle().background != 0) {
+			Bitmap bm = BitmapFactory.decodeResource(crouton.getActivity().getResources(),
+					crouton.getStyle().background);
+			BitmapDrawable bd = new BitmapDrawable(crouton.getActivity().getResources(), bm);
+			
+			if (crouton.getStyle().tile)
+				bd.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+			
+			holder.background.setBackgroundDrawable(bd);
+		} else {
+			holder.background.setBackgroundDrawable(null);
+		}
+			
+		return holder.view;
+	}
+
+	private void initView(Crouton crouton) {
+		view = new FrameLayout(crouton.getActivity());
+		text = new TextView(crouton.getActivity());
+		background = new ImageView(crouton.getActivity());
+		//TODO eitan
+		LayoutParams lp = new LayoutParams(
+				LayoutParams.MATCH_PARENT, crouton.getStyle().height);
+		FrameLayout.LayoutParams lp2 = new FrameLayout.LayoutParams(lp);
+		lp2.topMargin = crouton.getYMargin();
+		lp2.gravity = crouton.getGravity();
+		view.setLayoutParams(lp2);
+		
+		text.setText(crouton.getText());
+		text.setLayoutParams(sParams);
+		text.setTypeface(Typeface.DEFAULT_BOLD);
+		text.setPadding(PADDING, PADDING, PADDING, PADDING);
+		text.setGravity(Gravity.CENTER);
+		
+		background.setLayoutParams(sParams);
+		
+		view.addView(background);
+		view.addView(text);
+	}
+}
